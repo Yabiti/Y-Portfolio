3341f57e529c8c681022026ad6fb82430cce0efb
Yabiti <yabetseyohannes@gmail.com>
2026-05-09 09:45:22 +0100

    .

--- layouts/animation.tsx
+++ layouts/animation.tsx
@@ -1,24 +1,17 @@
 "use client"
+import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { useEffect } from 'react'
-import { usePathname } from 'next/navigation' // Add this
 
 export default function AnimationLayout({ children }: { children: React.ReactNode }) {
-    const pathname = usePathname(); // Track current URL
-
     useEffect(() => {
-        const initAOS = async () => {
-            const AOS = (await import("aos")).default;
-            AOS.init({
-                duration: 1000,
-                once: true,
-                mirror: false,
-                anchorPlacement: 'top-bottom',
-            });
-            AOS.refresh();
-        };
-        initAOS();
-    }, [pathname]); // 🔥 Re-run every time the page/URL changes
+        AOS.init({
+            duration: 1000,
+            once: true,
+            mirror: false,
+        });
+        AOS.refresh();
+    }, []);
 
     return <>{children}</>;
 }

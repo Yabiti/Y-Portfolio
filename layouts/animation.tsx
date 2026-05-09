a0aab36793728c25b9d2fd6cff9ded2a01ec70cc
Yabiti <yabetseyohannes@gmail.com>
2026-05-09 09:03:06 +0100

    .

--- layouts/animation.tsx
+++ layouts/animation.tsx
@@ -1,7 +1,23 @@
-
+"use client"
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
 
 export default function AnimationLayout(
     { children }:
-        { children: React.ReactNode }) {
+        { children: React.ReactNode     
+        }) {
+            useEffect(() => {
+                const initAOS = async () => {
+                    await import("aos");
+                    AOS.init({
+                        duration: 1000,
+                        easing: "ease",
+                        once: true,
+                        anchorPlacement: 'top-center'
+                    })
+                }
+                initAOS();
+            }, [])
     return <>{children}</>
 }
